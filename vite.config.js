b50ebc3f11ce8946dae326f989bc1ab98c82aa19
AndriiDorohov <wedpositive@gmail.com>
2025-05-22 14:30:13 +0200

    edited vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-vite.config.js;
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
-  server: {
-    historyApiFallback: true,
-  },
 });
